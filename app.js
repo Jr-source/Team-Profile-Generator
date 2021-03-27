3e15d6b18291aaf273c9f207099848fa65deb0c2
Jacob Riley <jacobriley1987@gmail.com>
2021-03-26 23:15:46 -0700

    run app.js success with Team.html

--- app.js
+++ app.js
@@ -10,27 +10,160 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = [];
 
+function askUserForManagerInfo() {
+  console.log("ASK FOR MANAGER INFO!");
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please provide Manager's Name!",
+        name: "name",
+      },
+      {
+        type: "number",
+        message: "Enter Manager's ID!",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter Manager's Email!",
+        name: "email",
+      },
+      {
+        type: "number",
+        message: "Enter Manager's Office Number!",
+        name: "officeNumber",
+      },
+    ])
+    .then((managerData) => {
+      const newManager = new Manager(
+        managerData.name,
+        managerData.id,
+        managerData.email,
+        managerData.officeNumber
+      );
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+      employeeList.push(newManager);
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+      askUserForEmployeeType();
+    });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function askUserForEmployeeType() {
+  console.log(" ");
+  console.log("EMPLOYEE TYPE!");
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't have any more member to add!"],
+        name: "employeeType",
+      },
+    ])
+    .then((newEmployeeChoiceData) => {
+      if (newEmployeeChoiceData.employeeType === "Engineer") {
+        askUserForEngineerInfo();
+      } else if (newEmployeeChoiceData.employeeType === "Intern") {
+        askUserForInternInfo();
+      } else {
+        createHTML();
+      }
+    });
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function askUserForEngineerInfo() {
+  console.log(" ");
+  console.log("ENGINEER INFO!");
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please provide Engineer's Name!",
+        name: "name",
+      },
+      {
+        type: "number",
+        message: "Enter Engineer's ID!",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter Engineer's Email!",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Enter Engineer's GitHub Username!",
+        name: "github",
+      },
+    ])
+    .then((engineerData) => {
+      const newEngineer = new Engineer(
+        engineerData.name,
+        engineerData.id,
+        engineerData.email,
+        engineerData.github
+      );
+
+      employeeList.push(newEngineer);
+
+      askUserForEmployeeType();
+    });
+}
+
+function askUserForInternInfo() {
+  console.log(" ");
+  console.log("INTERN INFO!");
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please provide Intern's Name!",
+        name: "name",
+      },
+      {
+        type: "number",
+        message: "Enter Intern's ID!",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter Intern's Email!",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Enter Intern's school!",
+        name: "school",
+      },
+    ])
+    .then((internData) => {
+      const newInter = new Intern(
+        internData.name,
+        internData.id,
+        internData.email,
+        internData.school
+      );
+
+      employeeList.push(newInter);
+
+      askUserForEmployeeType();
+    });
+}
+
+function createHTML() {
+  const htmlContent = render(employeeList);
+
+  fs.writeFile(outputPath, htmlContent, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("HTML file created");
+  });
+}
+
+askUserForManagerInfo();
